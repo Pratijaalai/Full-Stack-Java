d045dbc047dda7f72fd7f1a5768881f03c1580a4
Pratijaalai <168396021+Pratijaalai@users.noreply.github.com>
2025-03-04 11:31:21 +0530

    Update Assignment7.java

--- Week 1/Assignment7.java
+++ Week 1/Assignment7.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class AsciiValue {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0);  // Read the first character
+      
+        int asciiValue = (int) ch;
+        System.out.println("The ASCII value of '" + ch + "' is: " + asciiValue);
+      
+        scanner.close();
+    }
+}
 
