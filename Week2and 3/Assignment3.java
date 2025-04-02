0bee691f63192594c7a447018954e055124efeae
Pratijaalai <168396021+Pratijaalai@users.noreply.github.com>
2025-04-02 12:54:29 +0530

    Create Assignment3.java

---
+++ Week2and 3/Assignment3.java
@@ -0,0 +1,11 @@
+public class CharArrayToString {
+    public static void main(String[] args) {
+        char[] charArray = {'H', 'e', 'l', 'l', 'o'};
+        
+        // Convert char array to string
+        String str = new String(charArray);
+        
+        // Display the string
+        System.out.println("Converted String: " + str);
+    }
+}
